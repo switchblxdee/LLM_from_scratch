8d452989561ad60e9246a3601ec9526548e4c1d1
switchblxde <grechin3003@gmail.com>
2025-11-25 21:18:10 +0300

    added decoder

--- decoder.py
+++ decoder.py
@@ -19,3 +19,21 @@ class DecoderBlock(nn.Module):
         ffn_norm_h = self.ffn_norm(h)
         h = h + self.ffn(ffn_norm_h)
         return h
+
+class Decoder(nn.Module):
+    def __init__(self, n_layers, n_vocab, n_emb, n_hidden, n_heads, q_lora_rank, kv_lora_rank, nope_head_dim, rope_head_dim, dropout=0.0, max_len=1024):
+        super().__init__()
+        
+        self.embeddings = nn.Embedding(n_vocab, n_emb)
+        self.final_norm = RMSNorm(n_emb)
+        self.lm_head = nn.Linear(n_emb, n_vocab, bias=False)
+        
+        self.blocks = nn.ModuleList([DecoderBlock(n_emb, n_hidden, n_heads, q_lora_rank, kv_lora_rank, nope_head_dim, rope_head_dim, dropout, max_len) for _ in range(n_layers)])
+
+    def forward(self, x):
+        x = self.embeddings(x)
+        for block in self.blocks:
+            x = block(x)
+        x = self.final_norm(x)
+        logits = self.lm_head(x)
+        return logits
